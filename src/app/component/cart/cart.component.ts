9beeb29002228d7184ac66848ea040508291b9b0
Anurag Shrivastava <nrgshrivastava361@gmail.com>
2022-06-01 21:58:23 +0530

    initial commit

---
+++ src/app/component/cart/cart.component.ts
@@ -0,0 +1,38 @@
+import { NumberSymbol } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+
+  public products: any = [];
+  public grandTotal !: number;
+  constructor(private cartService: CartService, private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProducts()
+      .subscribe(res => {
+        this.products = res;
+        this.grandTotal = this.cartService.getTotalPrice();
+      })
+  }
+  removeItem(item: any) {
+    this.cartService.removeCartItem(item);
+  }
+  emptycart() {
+    this.cartService.removeAllCart();
+  }
+  quantity(item: any) {
+    let val = +(document.getElementById(String(item.id)) as HTMLInputElement).value;
+    this.cartService.update(item, val);
+  }
+  checkout() {
+    this.emptycart();
+  }
+
+}
---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public totalItem : number = 0;
+  public searchTerm !: string;
+  constructor(private cartService : CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+    })
+  }
+  search(){
+    this.cartService.search.next(this.searchTerm);
+  }
+}
